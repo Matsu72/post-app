a3bcc69ef326823b40be7c53edee75d42172a873
Matsu72 <naganao0702@outlook.jp>
2025-06-08 17:14:36 +0900

    Add post creation page

--- posts/views.py
+++ posts/views.py
@@ -1,5 +1,5 @@
 #HTMLを生成して返す
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import Post
 
 
@@ -9,7 +9,17 @@ def post_list(request):
     return render(request, 'posts/post_list.html', {'posts': posts})
 
 
-
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'posts/post_detail.html', {'post': post})
+
+
+def post_create(request):
+    if request.method == 'POST':
+        title = request.POST.get('title')
+        content = request.POST.get('content')
+        if title and content:
+            Post.objects.create(title=title, content=content)
+            return redirect('post_list')  # 投稿後は一覧に戻る
+    return render(request, 'posts/post_form.html')
+
