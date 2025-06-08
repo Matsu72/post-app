d2350368d603d9c6e8e082118bf3c4f3063d08fd
Matsu72 <naganao0702@outlook.jp>
2025-06-09 01:13:26 +0900

    本番用に settings.py を環境変数対応 & docker-compose に env_file を追加

--- config/settings.py
+++ config/settings.py
@@ -19,13 +19,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-n(p73x)a_&!1ez&h*o97s!6k^639xw=ie@^vu@88-&&sj@d&)&'
+import os
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+SECRET_KEY = os.environ.get('SECRET_KEY', 'dummy-key')
 
-ALLOWED_HOSTS = []
+DEBUG = os.environ.get('DEBUG', 'False') == 'True'
+
+ALLOWED_HOSTS = ['your-app-name.onrender.com']
 
 
 # Application definition
