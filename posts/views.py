7e53b0a7ee5363810e1d27dc7a8dd16989e1bd86
Matsu72 <naganao0702@outlook.jp>
2025-06-09 00:21:26 +0900

    Add post create/edit with ModelForm

--- posts/views.py
+++ posts/views.py
@@ -1,7 +1,7 @@
 #HTMLを生成して返す
 from django.shortcuts import render, get_object_or_404, redirect
 from .models import Post
-
+from .forms import PostForm
 
 # Create your views here.
 def post_list(request):
@@ -13,7 +13,7 @@ def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'posts/post_detail.html', {'post': post})
 
-
+"""
 def post_create(request):
     if request.method == 'POST':
         title = request.POST.get('title')
@@ -33,8 +33,34 @@ def post_edit(request, pk):
         return redirect('post_detail', pk=post.pk)
 
     return render(request, 'posts/post_form.html', {'post': post})
+"""
 
 def post_delete(request, pk):
     post = get_object_or_404(Post, pk=pk)
     post.delete()
     return redirect('post_list')
+
+
+# ModelForm版: 新規投稿
+def post_create_form(request):
+    if request.method == 'POST':
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm()
+    return render(request, 'posts/post_form_modelform.html', {'form': form})
+
+
+# ModelForm版: 編集
+def post_edit_form(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == 'POST':
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            post = form.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm(instance=post)
+    return render(request, 'posts/post_form_modelform.html', {'form': form})
